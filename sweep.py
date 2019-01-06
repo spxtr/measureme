eac46d8d965335685bf87118d7995ccfe1301c49
spxtr <spxtr@google.com>
2019-01-05 23:12:00 -0800

    Add gains to sr830s, tidy up a bit.

--- sweep.py
+++ sweep.py
@@ -17,52 +17,59 @@ def _autorange_srs(srs, max_changes=1):
         elif r < 0.1 * sens:
             return srs.decrement_sensitivity()
         return False
-
     sets = 0
     while autorange_once() and sets < max_changes:
         sets += 1
         time.sleep(10*srs.time_constant.get())
 
-class Sweep1D(object):
-    def __init__(self, set_param):
-        self._set_param = set_param
+class Sweep(object):
+    def __init__(self):
         self._sr830s = []
         self._params = []
-        self._meas = Measurement()
-        self._meas.register_parameter(set_param)
-        self._meas.register_custom_parameter('time', label='Time', unit='s')
     
     def follow_param(self, p):
-        self._meas.register_parameter(p, setpoints=(self._set_param, 'time',))
         self._params.append(p)
 
-    def follow_sr830(self, l, name):
-        self._meas.register_parameter(l.X, setpoints=(self._set_param, 'time',))
-        self._meas.register_parameter(l.Y, setpoints=(self._set_param, 'time',))
-        self._sr830s.append((l, name))
+    def follow_sr830(self, l, name, gain=1.0):
+        self._sr830s.append((l, name, gain))
+
+    def _create_measurement(self, *set_params):
+        meas = Measurement()
+        for p in set_params:
+            meas.register_parameter(p)
+        meas.register_custom_parameter('time', label='Time', unit='s')
+        for p in self._params:
+            meas.register_parameter(p, setpoints=(*set_params, 'time',))
+        for l, _, _ in self._sr830s:
+            meas.register_parameter(l.X, setpoints=(*set_params, 'time',))
+            meas.register_parameter(l.Y, setpoints=(*set_params, 'time',))
+        return meas
     
-    def run(self, vals, inter_delay=None):
+    def sweep(self, set_param, vals, inter_delay=None):
         if inter_delay is not None:
-            duration = len(vals)*inter_delay
-            print(f'Minimum duration: {int(duration/3600):d}h {int(duration/60 % 60)}m {int(duration % 60)}s')
+            d = len(vals)*inter_delay
+            h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
+            print(f'Minimum duration: {h}h {m}m {s}s')
+
         fig = plt.figure(figsize=(4*(2 + len(self._params) + len(self._sr830s)),4))
         grid = plt.GridSpec(4, 1 + len(self._params) + len(self._sr830s), hspace=0)
         setax = fig.add_subplot(grid[:, 0])
         setax.set_xlabel('Time (s)')
-        setax.set_ylabel(f'{self._set_param.label} ({self._set_param.unit})')
+        setax.set_ylabel(f'{set_param.label} ({set_param.unit})')
         setaxline = setax.plot([], [])[0]
-        fig.show()
+
         paxs = []
         plines = []
         for i, p in enumerate(self._params):
             ax = fig.add_subplot(grid[:, 1 + i])
-            ax.set_xlabel(f'{self._set_param.label} ({self._set_param.unit})')
+            ax.set_xlabel(f'{set_param.label} ({set_param.unit})')
             ax.set_ylabel(f'{p.label} ({p.unit})')
             paxs.append(ax)
             plines.append(ax.plot([], [])[0])
+
         laxs = []
         llines = []
-        for i, (l, name) in enumerate(self._sr830s):
+        for i, (l, name, _) in enumerate(self._sr830s):
             ax0 = fig.add_subplot(grid[:-1, 1 + len(self._params) + i])
             ax0.set_ylabel(f'{name} (V)')
             fmt = ScalarFormatter()
@@ -72,15 +79,20 @@ class Sweep1D(object):
             llines.append(ax0.plot([], [])[0])
             ax1 = fig.add_subplot(grid[-1, 1 + len(self._params) + i], sharex=ax0)
             ax1.set_ylabel('Phase (°)')
-            ax1.set_xlabel(f'{self._set_param.label} ({self._set_param.unit})')
+            ax1.set_xlabel(f'{set_param.label} ({set_param.unit})')
             laxs.append(ax1)
             llines.append(ax1.plot([], [])[0])
             plt.setp(ax0.get_xticklabels(), visible=False)
-        with self._meas.run() as datasaver:
+
+        fig.tight_layout()
+        fig.show()
+
+        meas = self._create_measurement(set_param)
+        with meas.run() as datasaver:
             t0 = time.monotonic()
             for setpoint in vals:
                 t = time.monotonic() - t0
-                self._set_param.set(setpoint)
+                set_param.set(setpoint)
                 
                 setaxline.set_xdata(np.append(setaxline.get_xdata(), t))
                 setaxline.set_ydata(np.append(setaxline.get_ydata(), setpoint))
@@ -89,8 +101,9 @@ class Sweep1D(object):
                 
                 if inter_delay is not None:
                     plt.pause(inter_delay)
+
                 data = [
-                    (self._set_param, setpoint),
+                    (set_param, setpoint),
                     ('time', t)
                 ]
                 for i, p in enumerate(self._params):
@@ -100,9 +113,10 @@ class Sweep1D(object):
                     plines[i].set_ydata(np.append(plines[i].get_ydata(), v))
                     paxs[i].relim()
                     paxs[i].autoscale_view()
-                for i, (l, _) in enumerate(self._sr830s):
+                for i, (l, _, gain) in enumerate(self._sr830s):
                     _autorange_srs(l, 3)
                     x, y = l.snap('x', 'y')
+                    x, y = x / gain, y / gain
                     data.extend([(l.X, x), (l.Y, y)])
                     llines[i*2].set_xdata(np.append(llines[i*2].get_xdata(), setpoint))
                     llines[i*2].set_ydata(np.append(llines[i*2].get_ydata(), x))
@@ -118,6 +132,11 @@ class Sweep1D(object):
                 fig.tight_layout()
                 fig.canvas.draw()
                 plt.pause(0.001)
+
+            d = time.monotonic() - t0
+            h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
+            print(f'Completed in: {h}h {m}m {s}s')
+
             b = io.BytesIO()
             fig.savefig(b, format='png')
             display.display(display.Image(data=b.getbuffer(), format='png'))
