b390b2412bfb5e097ca388ed8715ab23402a0134
spxtr <spxtr@google.com>
2019-05-20 15:14:58 -0700

    Tidy up plotting stuff.

--- sweep.py
+++ sweep.py
@@ -44,61 +44,91 @@ class Sweep(object):
             meas.register_parameter(l.X, setpoints=(*set_params, 'time',))
             meas.register_parameter(l.Y, setpoints=(*set_params, 'time',))
         return meas
-    
+
+    def _prepare_1d_plots(self, set_param):
+        self._fig = plt.figure(figsize=(4*(2 + len(self._params) + len(self._sr830s)),4))
+        grid = plt.GridSpec(4, 1 + len(self._params) + len(self._sr830s), hspace=0)
+        self._setax = self._fig.add_subplot(grid[:, 0])
+        self._setax.set_xlabel('Time (s)')
+        self._setax.set_ylabel(f'{set_param.label} ({set_param.unit})')
+        self._setaxline = self._setax.plot([], [])[0]
+
+        self._paxs = []
+        self._plines = []
+        for i, (p, _) in enumerate(self._params):
+            ax = self._fig.add_subplot(grid[:, 1 + i])
+            ax.set_xlabel(f'{set_param.label} ({set_param.unit})')
+            ax.set_ylabel(f'{p.label} ({p.unit})')
+            self._paxs.append(ax)
+            self._plines.append(ax.plot([], [])[0])
+
+        self._laxs = []
+        self._llines = []
+        for i, (l, name, _) in enumerate(self._sr830s):
+            ax0 = self._fig.add_subplot(grid[:-1, 1 + len(self._params) + i])
+            ax0.set_ylabel(f'{name} (V)')
+            fmt = ScalarFormatter()
+            fmt.set_powerlimits((-3, 3))
+            ax0.get_yaxis().set_major_formatter(fmt)
+            self._laxs.append(ax0)
+            self._llines.append(ax0.plot([], [])[0])
+            ax1 = self._fig.add_subplot(grid[-1, 1 + len(self._params) + i], sharex=ax0)
+            ax1.set_ylabel('Phase (°)')
+            ax1.set_xlabel(f'{set_param.label} ({set_param.unit})')
+            self._laxs.append(ax1)
+            self._llines.append(ax1.plot([], [])[0])
+            plt.setp(ax0.get_xticklabels(), visible=False)
+
+        self._fig.tight_layout()
+        self._fig.show()
+
+    def _update_1d_setax(self, setpoint, t):
+        self._setaxline.set_xdata(np.append(self._setaxline.get_xdata(), t))
+        self._setaxline.set_ydata(np.append(self._setaxline.get_ydata(), setpoint))
+        self._setax.relim()
+        self._setax.autoscale_view()
+
+    def _update_1d_param(self, i, setpoint, value):
+        self._plines[i].set_xdata(np.append(self._plines[i].get_xdata(), setpoint))
+        self._plines[i].set_ydata(np.append(self._plines[i].get_ydata(), value))
+        self._paxs[i].relim()
+        self._paxs[i].autoscale_view()
+
+    def _update_1d_sr830(self, i, setpoint, x, y):
+        self._llines[i*2].set_xdata(np.append(self._llines[i*2].get_xdata(), setpoint))
+        self._llines[i*2].set_ydata(np.append(self._llines[i*2].get_ydata(), x))
+        self._llines[i*2+1].set_xdata(np.append(self._llines[i*2+1].get_xdata(), setpoint))
+        self._llines[i*2+1].set_ydata(np.append(self._llines[i*2+1].get_ydata(), np.arctan2(y, x) * 180 / np.pi))
+        self._laxs[i*2].relim()
+        self._laxs[i*2].autoscale_view()
+        self._laxs[i*2+1].relim()
+        self._laxs[i*2+1].autoscale_view()
+
+    def _redraw_1d_plot(self):
+        self._fig.tight_layout()
+        self._fig.canvas.draw()
+        plt.pause(0.001)
+
+    def _display_1d_plot(self):
+        b = io.BytesIO()
+        self._fig.savefig(b, format='png')
+        display.display(display.Image(data=b.getbuffer(), format='png'))
+
     def sweep(self, set_param, vals, inter_delay=None):
         if inter_delay is not None:
             d = len(vals)*inter_delay
             h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
             print(f'Minimum duration: {h}h {m}m {s}s')
 
-        fig = plt.figure(figsize=(4*(2 + len(self._params) + len(self._sr830s)),4))
-        grid = plt.GridSpec(4, 1 + len(self._params) + len(self._sr830s), hspace=0)
-        setax = fig.add_subplot(grid[:, 0])
-        setax.set_xlabel('Time (s)')
-        setax.set_ylabel(f'{set_param.label} ({set_param.unit})')
-        setaxline = setax.plot([], [])[0]
-
-        paxs = []
-        plines = []
-        for i, (p, _) in enumerate(self._params):
-            ax = fig.add_subplot(grid[:, 1 + i])
-            ax.set_xlabel(f'{set_param.label} ({set_param.unit})')
-            ax.set_ylabel(f'{p.label} ({p.unit})')
-            paxs.append(ax)
-            plines.append(ax.plot([], [])[0])
-
-        laxs = []
-        llines = []
-        for i, (l, name, _) in enumerate(self._sr830s):
-            ax0 = fig.add_subplot(grid[:-1, 1 + len(self._params) + i])
-            ax0.set_ylabel(f'{name} (V)')
-            fmt = ScalarFormatter()
-            fmt.set_powerlimits((-3, 3))
-            ax0.get_yaxis().set_major_formatter(fmt)
-            laxs.append(ax0)
-            llines.append(ax0.plot([], [])[0])
-            ax1 = fig.add_subplot(grid[-1, 1 + len(self._params) + i], sharex=ax0)
-            ax1.set_ylabel('Phase (°)')
-            ax1.set_xlabel(f'{set_param.label} ({set_param.unit})')
-            laxs.append(ax1)
-            llines.append(ax1.plot([], [])[0])
-            plt.setp(ax0.get_xticklabels(), visible=False)
-
-        fig.tight_layout()
-        fig.show()
-
+        self._prepare_1d_plots(set_param)
         meas = self._create_measurement(set_param)
         with meas.run() as datasaver:
             t0 = time.monotonic()
             for setpoint in vals:
                 t = time.monotonic() - t0
                 set_param.set(setpoint)
-                
-                setaxline.set_xdata(np.append(setaxline.get_xdata(), t))
-                setaxline.set_ydata(np.append(setaxline.get_ydata(), setpoint))
-                setax.relim()
-                setax.autoscale_view()
-                
+                self._update_1d_setax(setpoint, t)
+
                 if inter_delay is not None:
                     plt.pause(inter_delay)
 
@@ -110,34 +140,21 @@ class Sweep(object):
                     v = p.get()
                     v = v / gain
                     data.append((p, v))
-                    plines[i].set_xdata(np.append(plines[i].get_xdata(), setpoint))
-                    plines[i].set_ydata(np.append(plines[i].get_ydata(), v))
-                    paxs[i].relim()
-                    paxs[i].autoscale_view()
+                    self._update_1d_param(i, setpoint, v)
+
                 for i, (l, _, gain) in enumerate(self._sr830s):
                     _autorange_srs(l, 3)
                     x, y = l.snap('x', 'y')
                     x, y = x / gain, y / gain
                     data.extend([(l.X, x), (l.Y, y)])
-                    llines[i*2].set_xdata(np.append(llines[i*2].get_xdata(), setpoint))
-                    llines[i*2].set_ydata(np.append(llines[i*2].get_ydata(), x))
-                    llines[i*2+1].set_xdata(np.append(llines[i*2+1].get_xdata(), setpoint))
-                    llines[i*2+1].set_ydata(np.append(llines[i*2+1].get_ydata(), np.arctan2(y, x) * 180 / np.pi))
-                    laxs[i*2].relim()
-                    laxs[i*2].autoscale_view()
-                    laxs[i*2+1].relim()
-                    laxs[i*2+1].autoscale_view()
+                    self._update_1d_sr830(i, setpoint, x, y)
 
                 datasaver.add_result(*data)
                 
-                fig.tight_layout()
-                fig.canvas.draw()
-                plt.pause(0.001)
+                self._redraw_1d_plot()
 
             d = time.monotonic() - t0
             h, m, s = int(d/3600), int(d/60) % 60, int(d) % 60
             print(f'Completed in: {h}h {m}m {s}s')
 
-            b = io.BytesIO()
-            fig.savefig(b, format='png')
-            display.display(display.Image(data=b.getbuffer(), format='png'))
+            self._display_1d_plot()
